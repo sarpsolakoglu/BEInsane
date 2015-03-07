fae15ab45b7d5c5ec38f6e729656d5a8f8bbd236
Sarp Ogulcan Solakoglu <sosolakoglu@gmail.com>
2015-03-07 10:26:31 +0200

    Initial Commit

---
+++ Beinsane/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Beinsane
+//
+//  Created by Sarp Ogulcan Solakoglu on 07/03/15.
+//  Copyright (c) 2015 nodious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Beinsane/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Beinsane
+//
+//  Created by Sarp Ogulcan Solakoglu on 07/03/15.
+//  Copyright (c) 2015 nodious. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
